eec5cee26e9c53b77fdd737772ac39ac432e8e4d
evgsavelov <savelov08@ya.ru>
2020-05-13 11:56:57 +0300

    correct

---
+++ Bonus_ruble copy.java
@@ -0,0 +1,8 @@
+public class Bonus_ruble {
+    public static void main(String[] args) {
+      int a = 100; //first balance
+      int b = 1100; //money for balance
+        int c = a + b + b/100; //bonus
+        System.out.println(c);
+    }
+}
