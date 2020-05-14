39af347528d16b7505dc522a7ec1b0bbda92a5ec
evgsavelov <savelov08@ya.ru>
2020-05-14 13:09:29 +0300

    correct_3

--- BonusRuble.java
+++ BonusRuble.java
@@ -1,8 +1,8 @@
 public class BonusRuble {
     public static void main(String[] args) {
-      int balance = 100; //first balance
-      int balance_new = 1100; //money for balance
-        int bonus = balance + balance_new + balance_new/100; //bonus
+        int balance = 100; //first balance
+        int balanceNew = 1100; //money for balance
+        int bonus = balance + balanceNew + balanceNew / 100; //bonus
         System.out.println(bonus);
     }
 }
