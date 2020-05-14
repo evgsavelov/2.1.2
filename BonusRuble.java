c274c760ab400615ddea61d683225973902b5df5
evgsavelov <savelov08@ya.ru>
2020-05-14 22:17:31 +0300

    adding if else

--- BonusRuble.java
+++ BonusRuble.java
@@ -1,8 +1,14 @@
 public class BonusRuble {
     public static void main(String[] args) {
         int balance = 100; //first balance
-        int balanceNew = 1100; //money for balance
-        int bonus = balance + balanceNew + balanceNew / 100; //bonus
-        System.out.println(bonus);
+        int balanceNew = 500; //money for balance
+        if (balanceNew > 1000) {
+            int bonus = balance + balanceNew + balanceNew / 100; //bonus
+            System.out.println(bonus);
+        } else {
+            int bonus = balance + balanceNew;
+            System.out.println(bonus);
+        }
     }
 }
+
